3c4efa0ff08e7b5a702bc0767a24976f7b992ad6
fesids <felipesousa458999@gmail.com>
2024-10-28 20:43:52 -0300

    fix: duplicate anfular template folder

--- angular-template/src/app/store/reducers/app.reducers.ts
+++
@@ -1,7 +0,0 @@
-import { ActionReducerMap } from '@ngrx/store';
-import { AppState } from '../states/app.state';
-import { authReducer } from './auth.reducer';
-
-export const appReducers: ActionReducerMap<AppState, any> = {
-    auth: authReducer
-}
